d29720cdc0666a2654e10bd30e2e6be6905b5f9f
Roberto Soto <rsotolei@gmail.com>
2022-10-03 06:19:36 -0500

    Approaching MVP

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Prueba_t360c
 
-## Install
+## Instalación e inicio
+
+### Instalar requerimientos de python con el siguiente comando
 
 > pip install -r requirements.txt
+
+### Guardar el archivo .env
+
+Si tiene autorización para usar esta aplicación, se le debe haber hecho llegar un archivo ".env" por otro medio. Simplemente guárdelo en el directorio root del proyecto (el mismo que contiene el archivo "server.py")
+
+### Iniciar el servidor
+
+> flask --app server run
+
--- server.py
+++ server.py
@@ -4,60 +4,122 @@ from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, request, render_template, session, url_for
 
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
-    load_dotenv(ENV_FILE)
-    
+	load_dotenv(ENV_FILE)
+	
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
 
 oauth = OAuth(app)
 
 oauth.register(
-    "auth0",
-    client_id=env.get("AUTH0_CLIENT_ID"),
-    client_secret=env.get("AUTH0_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+	"auth0",
+	client_id=env.get("AUTH0_CLIENT_ID"),
+	client_secret=env.get("AUTH0_CLIENT_SECRET"),
+	client_kwargs={
+		"scope": "openid profile email",
+	},
+	server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
 )
 
+stays = []
 
 @app.route("/login")
 def login():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
+	return oauth.auth0.authorize_redirect(
+		redirect_uri=url_for("callback", _external=True)
+	)
 
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
-    token = oauth.auth0.authorize_access_token()
-    session["user"] = token
-    return redirect("/")
+	token = oauth.auth0.authorize_access_token()
+	session["user"] = token
+	return redirect("/")
 
 @app.route("/logout")
 def logout():
-    session.clear()
-    return redirect(
-        "https://" + env.get("AUTH0_DOMAIN")
-        + "/v2/logout?"
-        + urlencode(
-            {
-                "returnTo": url_for("home", _external=True),
-                "client_id": env.get("AUTH0_CLIENT_ID"),
-            },
-            quote_via=quote_plus,
-        )
-    )
+	session.clear()
+	return redirect(
+		"https://" + env.get("AUTH0_DOMAIN")
+		+ "/v2/logout?"
+		+ urlencode(
+			{
+				"returnTo": url_for("home", _external=True),
+				"client_id": env.get("AUTH0_CLIENT_ID"),
+			},
+			quote_via=quote_plus,
+		)
+	)
 
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+	"""
+	if len(stays) > 0:
+		return render_template("home.html", session=session.get('user'), stays=stays)
+	else:
+		return render_template("home.html", session=session.get('user'))
+	"""
+	return render_template("home.html", session=session.get('user'), stays=stays)
 
+@app.route("/test")
+def test():
+	data = [
+		{
+			"title": "A",
+			"location": "Mexico City"
+		},
+		{
+			"title": "B",
+			"location": "Paris"
+		},
+		{
+			"title": "C",
+			"location": "Tokyo"
+		}
+	]
+	return render_template("test.html", table=data)
+
+@app.post("/addStay")
+def addStay():
+	obj = {
+		"title": request.form.get("title"),
+		"location": request.form.get("location"),
+		"status": "Available",
+		"owner": {
+			"email": request.form.get("useremail"),
+			"nickname": request.form.get("usernickname")
+		}
+	}
+	stays.append(obj)
+	return redirect(url_for("home"))
+
+@app.get("/reserveStay")
+def reserveStay():
+	for i in range(len(stays)):
+		if stays[i]["title"] == request.args.get("title"):
+			stays[i]["guest"] = {
+				"name": request.args.get("guestname"),
+				"email": request.args.get("guestemail")
+			}	
+			stays[i]["status"] = "Unavailable"
+			break
+	return redirect(url_for("home"))
+
+@app.get("/deleteStay")
+def deleteStay():
+	for i in range(len(stays)):
+		if stays[i]["title"] == request.args.get("title"):
+			del stays[i]
+			break
+	return redirect(url_for("home"))
+
+@app.get("/gettest")
+def gettest():
+	return request.args.get("hello")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
+	app.run(host="0.0.0.0", port=env.get("PORT", 3000))
